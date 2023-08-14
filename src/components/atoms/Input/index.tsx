84f857e5f7eaa3753555e7c77414b27f46e13463
otobakataro <vancyroletaka@gmail.com>
2023-08-14 21:45:05 +0900

    add input component and storybook

---
+++ src/components/atoms/Input/index.tsx
@@ -0,0 +1,42 @@
+import styled, { css } from 'styled-components'
+const Input = styled.input<{ hasError?: boolean; hasBorder?: boolean}>`
+    color: ${({ theme }) => theme.color.inputText};
+    ${({ theme, hasBorder, hasError }) => {
+        if (hasBorder) {
+            return css`
+                border: 1px solid
+                ${hasError ? theme.colors.danger : theme.colors.border};
+            `
+        } else {
+            return css`
+                border: none;
+            `
+        }
+    }}
+    padding: 11px 12px 12px 9px;
+    box-sizing: border-box;
+    outline: none;
+    width: 100%;
+    height: 38%;
+    font-size: 16px;
+    line-height: 19px;
+
+    &::placeholder {
+        color: ${({ theme }) => theme.colors.placeholder}
+    }
+    $::-webkit-outer-spin-button,
+    $::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    &[type='number'] {
+        -moz-appearance: textfield;
+    }
+`
+
+Input.defaultProps = {
+    hasBorder: true,
+}
+
+export default Input
