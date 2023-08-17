6328d709ca572b5ad57d675cddbe7a1dd1f03027
otobakataro <vancyroletaka@gmail.com>
2023-08-17 21:44:24 +0900

    add dropdown component and storybook

--- src/components/molecules/DropDown/index.tsx
+++ src/components/molecules/DropDown/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef, useCallback } from 'react'
 import styled from 'styled-components'
 import Text from 'components/atoms/Text'
 import Flex from 'components/layout/Flex'
@@ -51,4 +51,138 @@ const DropdownArrow = styled.div<{ isOpen?: boolean }>`
 const DropdownMenu = styled.div`
     background-color: #ffffff;
     border: ${({ theme }) => theme.colors.border};
+    box-shadow: 0px 5px 5px -3px rgb(0 0 0 / 20%), 0px 8px 10px 1px rgb(0 0 0 / 10%), 0px 3px 14px 2px rgb(0 0 0 / 12%);
+    box-sizing: border-box;
+    border-radius: 5px;
+    margin-top: -1px;
+    max-heihgt: 200px;
+    overflow-y: auto;
+    position: absolute;
+    top: 100%;
+    width: 100%;
+    z-index: 1000;
 `
+
+const DropdownOption = styled.div`
+    padding: 8px 12px 8px 12px;
+    &:hover {
+        background-color: #f9f9f9;
+    }
+`
+
+interface DropdownItemProps {
+    item: DropdownItem
+}
+
+const DropdownItem = (props: DropdownItemProps) => {
+    const { item } = props
+
+    return (
+        <Flex alignItems="center">
+            <Text margin={0} variant="small">
+                {item.label ?? item.value}
+            </Text>
+        </Flex>
+    )
+}
+
+export interface DropdownItem {
+    value: string | number | null
+    label?: string
+}
+
+interface DropdownProps {
+    options: DropdownItem[]
+    value?: string | number
+    name?: string
+    placeholder?: string
+    hasError?: boolean
+    onChange?: (selected?: DropdownItem) => void
+}
+
+const Dropdown = (props: DropdownProps) => {
+    const { onChange, name, options, hasError } = props
+    const initialItem = options.find((i) => i.value === props.value)
+    const [isOpen, setIsOpenValue] = useState(false)
+    const [selectedItem, setSelectedItem] = useState(initialItem)
+    const dropdownRef = useRef<HTMLDivElement>(null)
+
+    const handleDocumentClick = useCallback((e: MouseEvent | TouchEvent) => {
+        if (dropdownRef.current) {
+            const elems = dropdownRef.current.querySelectorAll('*')
+
+            for (let i = 0; i < elems.length; i++) {
+                if (elems[i] == e.target) {
+                    return
+                }
+            }
+        }
+        setIsOpenValue(false)
+    },
+    [dropdownRef],
+    )
+
+    const handleMouseDown = (e: React.SyntheticEvent) => {
+        setIsOpenValue((isOpen) => !isOpen)
+        e.stopPropagation()
+    }
+
+    const handleSelectValue = (e: React.FormEvent<HTMLDivElement>, item: DropdownItem) => {
+        e.stopPropagation()
+
+        setSelectedItem(item)
+        setIsOpenValue(false)
+        onChange && onChange(item)
+    }
+
+    useEffect(() => {
+        document.addEventListener('click', handleDocumentClick, false)
+        document.addEventListener('touchend', handleDocumentClick, false)
+
+        return function cleanup() {
+            document.removeEventListener('click', handleDocumentClick, false)
+            document.removeEventListener('touchend', handleDocumentClick, false)
+        }
+    }, [])
+
+    return (
+        <DropdownRoot ref={dropdownRef}>
+            <DropdownControl
+                hasError={hasError}
+                onMouseDown={handleMouseDown}
+                onTouchEnd={handleMouseDown}
+            >
+                {selectedItem && (
+                    <DropdownValue>
+                        <DropdownItem item={selectedItem} />
+                    </DropdownValue>
+                )}
+                {!selectedItem && (
+                    <DropdownPlaceholder>{props?.placeholder}</DropdownPlaceholder>
+                )}
+                <input
+                    type="hidden"
+                    name={name}
+                    value={selectedItem?.value ?? ''}
+                    onChange={() => onChange && onChange(selectedItem)}
+                />
+                <DropdownArrow isOpen={isOpen} />
+            </DropdownControl>
+            {isOpen && (
+                <DropdownMenu>
+                    {props.options.map((item, idx) => (
+                        <DropdownOption
+                            key={idx}
+                            onMouseDown={(e) => handleSelectValue(e, item)}
+                            onClick={(e) => handleSelectValue(e, item)}
+                        >
+                            <DropdownItem item={item} />
+                        </DropdownOption>
+                    ))}
+                </DropdownMenu>
+            )}
+        </DropdownRoot>
+    )
+}
+
+export default Dropdown
