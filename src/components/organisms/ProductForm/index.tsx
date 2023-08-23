d4c6e7a6c24c3aa6ffd5b7445ce97d195b7956c6
otobakataro <vancyroletaka@gmail.com>
2023-08-23 22:13:07 +0900

    WIP add ProductFrom component

---
+++ src/components/organisms/ProductForm/index.tsx
@@ -0,0 +1,137 @@
+import { Controller, useForm } from 'react-hook-form'
+import Button from 'components/atoms/Button'
+import Input from 'components/atoms/Input'
+import Text from 'components/atoms/Text'
+import TextArea from 'components/atoms/TextArea'
+import Box from 'components/layout/Box'
+import Dropdown from 'components/molecules/DropDown'
+import InputImages, { FileData } from 'components/molecules/InputImages'
+import type { Category, Condition } from 'types/data'
+
+export type ProductFormData = {
+    image: FileData[]
+    title: string
+    description: string
+    category: Category
+    condition: Condition
+    proce: string
+}
+
+interface ProductFormProps {
+    onProductSave?: (data: ProductFormData) => void
+}
+
+const ProductForm = ({ onProductSave }: ProductFormProps) => {
+    const { register, handleSubmit, control, formState: { errors },} = useForm<ProductFormData>()
+
+    const onSubmit = (data: ProductFormData) => {
+        onProductSave && onProductSave(data)
+    }
+
+    return (
+        <form>
+            <Box marginBottom={3}>
+                <Box marginBottom={2}>
+                    <Text as="label" variant="mediumLarge" fontWeight="bold">
+                        商品の写真
+                    </Text>
+                </Box>
+                <Controller
+                    control={control}
+                    name="image"
+                    rules={{ required: true }}
+                    render={({ field: { onChange, value }, fieldState: { error } }) => (
+                        <InputImages
+                            images={value ?? []}
+                            onChange={onChange}
+                            maximumNumber={1}
+                            hasError={!!error}
+                        />
+                    )}
+                />
+                {errors.image && (
+                    <Text color="danger" variant="small" paddingLeft={1}>
+                        Product image is required
+                    </Text>
+                )}
+            </Box>
+            <Box marginBottom={3}>
+                <Box marginBottom={2}>
+                    <Text as="label" variant="mediumLarge" fontWeight="bold">
+                        商品情報
+                    </Text>
+                </Box>
+            </Box>
+            <Box marginBottom={1}>
+                <Text as="label" variant="medium">
+                    タイトル
+                </Text>
+                <Input
+                    {...register("title", {required: true})}
+                    name="title"
+                    type="text"
+                    placeholder="商品のタイトル"
+                    hasError={!!errors.title}
+                />
+                {errors.title && (
+                    <Text color="danger" variant="small" paddingLeft={1}>
+                        タイトルの入力は必須です
+                    </Text>
+                )}
+            </Box>
+            <Box marginBottom={1}>
+                <Text as="label" variant="medium">
+                    概要
+                </Text>
+                <Controller
+                    control={control}
+                    name="description"
+                    rules={{ required: true }}
+                    render={({ field: { onChange, value}, fieldState: { error }}) => (
+                        <TextArea
+                            placeholder='最高の商品です!'
+                            hasError={!!error}
+                            onChange={onChange}
+                        >
+                            {value}
+                        </TextArea>
+                    )}
+                />
+                {errors.description && (
+                    <Text color="danger" variant="small" paddingLeft={1}>
+                        概要の入力は必須です
+                    </Text>
+                )}
+            </Box>
+            <Box marginBottom={1}>
+                <Text as="label" variant='medium'>
+                    カテゴリ
+                </Text>
+                <Controller
+                    control={control}
+                    name="category"
+                    rules={{ required: true }}
+                    defaultValue="shoes"
+                    render={({ field: { onChange, value }, fieldState: { error }}) => (
+                        <Dropdown
+                            options={[
+                                { value: 'shoes', label: 'シューズ' },
+                                { value: 'clothes', label: 'トップス' },
+                                { value: 'book', label: '本' },
+                            ]}
+                            hasError={!!error}
+                            value={value}
+                            placeholder='カテゴリを選択してください'
+                            onChange={(v) => onChange(v?.value)}
+                        />
+                    )}
+                />
+                {errors.category && (
+                    <Text color="danger" variant="small" paddingLeft={1}>
+                        カテゴリの選択は必須です
+                    </Text>
+                )}
+            </Box>
+        </form>
+    )
+}
