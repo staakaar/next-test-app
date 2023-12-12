9c0227f7d78ec67bc1c19ceaee5d7f57a6c10d1b
staakaar <vancyroletaka@gmail.com>
2023-12-13 01:17:39 +0900

    fix lyaout

--- src/components/organisms/MenuPopover/index.tsx
+++ src/components/organisms/MenuPopover/index.tsx
@@ -1,5 +1,5 @@
 import { Popover, Transition } from "@headlessui/react";
-import { Button, Link, MenuItem, MenuList, Paper } from "@mui/material";
+import { Button, Link, Menu, MenuItem, MenuList, Paper } from "@mui/material";
 import { useState } from "react";
 
 interface Category {
@@ -26,44 +26,56 @@ const MenuPopover = (/**categories: Category[]*/) => {
     const [isMenuPopover, setIsMenuPopover] = useState(false);
 
     return (
-        <Popover className="relative">
-            {({ open }) => (
-                <>
-                    <Popover.Button
-                        className={`border-none group inline-flex items-center rounded-md bg-orange-700 px-3 py-2 text-base font-medium hover:text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75`}
-                        onMouseEnter={() => setIsMenuPopover(true)}
-                    >
-                        {/* <Button> */}
-                            すべて
-                        {/* </Button> */}
-                    </Popover.Button>
-                    {isMenuPopover && (
-                        <Transition
-                            show={open}
-                            enter="transition duration-100 ease-out"
-                            enterFrom="transform scale-95 opacity-0"
-                            enterTo="transform scale-100 opacity-100"
-                            leave="transition duration-75 ease-out"
-                            leaveFrom="transform scale-100 opacity-100"
-                            leaveTo="transform scale-95 opacity-0"
+        <div>
+            <Popover className="relative">
+                {({ open }) => (
+                    <>
+                        <Popover.Button
+                            className="border-none inline-flex items-center rounded-m px-3 py-2 text-base font-medium"
+                            onMouseEnter={() => setIsMenuPopover(true)}
                         >
-                            <Popover.Panel static>
-                                <Paper>
-                                    {categories.map((category) => (
-                                            <MenuList>
-                                                <MenuItem><Link key={category.id}>aa</Link></MenuItem>
-                                                <MenuItem><Link></Link></MenuItem>
-                                                <MenuItem><Link></Link></MenuItem>
-                                            </MenuList>
-                                        
-                                    ))}
-                                </Paper>
-                            </Popover.Panel>
-                        </Transition>
-                    )}
-                </>
-            )}
-        </Popover>
+                            {/* <Button> */}
+                                すべて
+                            {/* </Button> */}
+                        </Popover.Button>
+                        {isMenuPopover && (
+                            <Transition
+                                show={open}
+                                enter="transition duration-100 ease-out"
+                                enterFrom="transform scale-95 opacity-0"
+                                enterTo="transform scale-100 opacity-100"
+                                leave="transition duration-75 ease-out"
+                                leaveFrom="transform scale-100 opacity-100"
+                                leaveTo="transform scale-95 opacity-0"
+                            >
+                                <Popover.Panel
+                                    className="
+                                        absolute
+                                        mt-3
+                                        w-screen
+                                        max-w-sm
+                                        -translate-x-1/2
+                                        transform
+                                        px-4 
+                                        sm:px-0
+                                        lg:max-w-3xl"
+                                >
+                                    <Menu>
+                                        {categories.map((category) => (
+                                            <MenuItem className="w-4 h-4">
+                                                <Link key={category.id}>
+                                                    testtest
+                                                </Link>
+                                            </MenuItem>
+                                        ))}
+                                    </Menu>
+                                </Popover.Panel>
+                            </Transition>
+                        )}
+                    </>
+                )}
+            </Popover>
+        </div>
     )
 }
 
--- src/components/organisms/SideMenu/index.tsx
+++ src/components/organisms/SideMenu/index.tsx
@@ -2,27 +2,34 @@ import Link from 'next/link'
 import Grid from "components/layout/Grid"
 import styled from "styled-components";
 import Box from 'components/layout/Box';
+import { MenuItem, MenuList, Paper, Stack } from '@mui/material';
 
 const SideMenuGrid = styled.div`
     width: 200px;
     display: block;
     background-color: gray;
-    grid-column-start: 1;
-    grid-column-end: 2;
-    grid-row-start: 2;
-    grid-row-end: 3;
+    padding: 20px 0;
 `
 
 const SideMenu = () => {
     return (
-        <SideMenuGrid>
-            <Box display={{ base: 'block', md: 'block' }}>
-                <Link href={'/products'}>商品一覧</Link>
-            </Box>
-            <Box display={{ base: 'block', md: 'block' }}>
-                <Link href={'/'}>カート一覧</Link>
-            </Box>
-        </SideMenuGrid>
+        <Stack direction="column" sx={{ width: '270px', margin: '30px', zIndex: "-1" }}>
+            <Paper variant='outlined'>
+                <MenuList className="w-64 h-80">
+                    <MenuItem divider><Link href={'/products'}>商品一覧</Link></MenuItem>
+                    <MenuItem divider sx={{ height: '40px' }}><Link href={'/'}>カート一覧</Link></MenuItem>
+                    <MenuItem>その他</MenuItem>
+                </MenuList>
+            </Paper>
+        </Stack>
+        // <SideMenuGrid>
+        //     <Box>
+        //         <Link href={'/products'}>商品一覧</Link>
+        //     </Box>
+        //     <Box>
+        //         <Link href={'/'}>カート一覧</Link>
+        //     </Box>
+        // </SideMenuGrid>
     )
 }
 
