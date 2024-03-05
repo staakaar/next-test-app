dd57248674e4de1e044bccc8f44f4a60fb0be8fe
staakaar <vancyroletaka@gmail.com>
2024-03-06 01:36:18 +0900

    add cors providesTags

--- src/pages/api/product/product.ts
+++ src/pages/api/product/product.ts
@@ -1,19 +1,19 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { context } from '../api';
 import { Product } from 'types';
+import { messageCreated } from 'lib/stores/common';
 
 const productsApi = createApi({
     reducerPath: 'products',
     baseQuery: fetchBaseQuery({
-        baseUrl: context.apiRootUrl
+        baseUrl: context.apiRootUrl,
+        prepareHeaders: async (headers, _) => {},
+        mode: 'cors',
     }),
-    tagTypes: ["Product", "UsersProduct"],
+    tagTypes: ["Product"],
     endpoints(builder) {
         return {
             removeProduct: builder.mutation({
-                invalidatesTags: (result, error, product) => {
-                    return [{ type: 'Product', id: product.id }]
-                },
                 query: (product) => {
                     return {
                         url: `/products/${product.id}`,
@@ -22,9 +22,6 @@ const productsApi = createApi({
                 },
             }),
             addProduct: builder.mutation({
-                invalidatesTags: (result, error, user) => {
-                    return [{ type: 'UsersProduct', id: user.id }];
-                },
                 query: (product) => {
                     return {
                         url: '/products',
@@ -36,24 +33,25 @@ const productsApi = createApi({
                     }
                 }
             }),
-            fetchProducts: builder.query({
-                providesTags: (result, error, user) => {
-                    const tags = result.map((product: Product) => {
-                        return { type: 'Product', id: product.id }
-                    });
-                    tags.push({ type: 'UsersProduct', id: user.id });
-                    return tags;
-                },
-                query: () => {
-                    return {
-                        url: '/products',
-                        method: 'GET',
-                        headers: {
-                            Accept: 'application/json',
-                            'Context-Type': 'application/json'
-                        }
-                    };
-                },
+            fetchProducts: builder.query<Product[], void>({
+                query: () => '/products',
+                transformResponse: (response: Product[]) => response,
+                transformErrorResponse: (response: { status: string | number }, meta, arg) => response.status,
+                providesTags: (results) => results
+                ? [
+                    ...results.map(({ id }) => ({ type: 'Product' as const, id })),
+                    { type: 'Product', id: 'LIST' },
+                ]
+                : [{ type: 'Product', id: 'LIST' }],
+                // async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+                //     dispatch(messageCreated('Fetching product...'))
+                //     try {
+                //         const { data } = await queryFulfilled
+                //         dispatch(messageCreated('product received!'))
+                //     } catch (err) {
+                //         dispatch(messageCreated('error Fetching product'))
+                //     }
+                // }
             }),
         }
     }
